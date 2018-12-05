c0c9ffa248d145baf6de283ff38d450e8d323ddf
17937458081 <45580217+17937458081@users.noreply.github.com>
2018-12-05 10:25:56 +0800

    3

--- Dwelling.Demo/Dwelling.Model/Address.cs
+++ Dwelling.Demo/Dwelling.Model/Address.cs
@@ -9,7 +9,7 @@ namespace Dwelling.Model
   public  class Address
     {
         /// <summary>
-        /// 地址ID sgvdhhak,k就
+        /// 地址ID 
         /// </summary>
         public int Address_ID { get; set; }
         /// <summary>
