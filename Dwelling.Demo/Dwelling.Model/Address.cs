0ba53f02d4bdbdb74a354493a33f4b368d51f5ab
dlbayue6 <45560387+dlbayue6@users.noreply.github.com>
2018-12-05 09:53:30 +0800

    5

--- Dwelling.Demo/Dwelling.Model/Address.cs
+++ Dwelling.Demo/Dwelling.Model/Address.cs
@@ -9,7 +9,7 @@ namespace Dwelling.Model
   public  class Address
     {
         /// <summary>
-        /// 地址ID
+        /// 地址ID规范化
         /// </summary>
         public int Address_ID { get; set; }
         /// <summary>
