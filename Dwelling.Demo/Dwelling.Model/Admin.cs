eb3fbb94d45a1b0dc84fcb151ae6f3c325aa0316
dlbayue6 <45560387+dlbayue6@users.noreply.github.com>
2018-12-05 10:14:44 +0800

    Update Admin.cs

--- Dwelling.Demo/Dwelling.Model/Admin.cs
+++ Dwelling.Demo/Dwelling.Model/Admin.cs
@@ -1,4 +1,4 @@
-
+﻿
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -29,35 +29,4 @@ namespace Dwelling.Model
         public int Admin_Permission { get; set; }
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Dwelling.Model
-{
-    public class Admin
-    {
-        /// <summary>
-        ///管理员ID  王帅磊
-        /// </summary>
-        [Key]
-        public int Admin_ID { get; set; }
-        /// <summary>
-        /// 姓名
-        /// </summary>
-        public string Admin_Name { get; set; }
-        /// <summary>
-        /// 密码
-        /// </summary>
-        public string Admin_Password { get; set; }
-        /// <summary>
-        /// 身份
-        /// </summary>
-        public int Admin_Permission { get; set; }
-    }
-}
->>>>>>> 5dea37e4946c50fa5fca8135c45a580194121aa7
