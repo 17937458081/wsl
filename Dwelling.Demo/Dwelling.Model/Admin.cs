94a218a6227eae105103207a1475c210500c5704
dlbayue6 <45560387+dlbayue6@users.noreply.github.com>
2018-12-05 10:05:56 +0800

    Revert "4"
    This reverts commit 0b5bfff84be354697c383e6ae58fa88a96fb7619.

--- Dwelling.Demo/Dwelling.Model/Admin.cs
+++ Dwelling.Demo/Dwelling.Model/Admin.cs
@@ -1,4 +1,4 @@
-﻿
+<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -11,7 +11,7 @@ namespace Dwelling.Model
     public class Admin
     {
         /// <summary>
-        ///管理员ID  丁玲
+        ///管理员ID  合适的话高大上对方是梵蒂冈
         /// </summary>
         [Key]
         public int Admin_ID { get; set; }
