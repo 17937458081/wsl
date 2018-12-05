5dea37e4946c50fa5fca8135c45a580194121aa7
17937458081 <45580217+17937458081@users.noreply.github.com>
2018-12-05 09:52:55 +0800

    2

--- Dwelling.Demo/Dwelling.Model/Admin.cs
+++ Dwelling.Demo/Dwelling.Model/Admin.cs
@@ -10,7 +10,7 @@ namespace Dwelling.Model
     public class Admin
     {
         /// <summary>
-        ///管理员ID  合适的话高大上对方是梵蒂冈
+        ///管理员ID  王帅磊
         /// </summary>
         [Key]
         public int Admin_ID { get; set; }
